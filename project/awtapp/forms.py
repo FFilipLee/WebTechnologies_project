65c235c824b8647f21fe23fec9807e57246b513d
Berkay Sahin <berkaysahindev@gmail.com>
2024-04-16 12:00:50 +0200

    ask question

--- project/awtapp/forms.py
+++ project/awtapp/forms.py
@@ -11,23 +11,13 @@ class RegistrationForm(UserCreationForm):
 
 
 class AskQuestionForm(forms.ModelForm):
+    user = forms.ModelChoiceField(queryset=User.objects.all(), label="Select User")
+
     class Meta:
         model = Question
-        fields = ['title', 'content']
-
-    def __init__(self, *args, **kwargs):
-        super(AskQuestionForm, self).__init__(*args, **kwargs)
-        self.fields['user'].queryset = User.objects.all()
-        #self.fields['user'].label_from_instance = lambda obj: "%s" % obj.get_full_name()  # Customize this as per your User model
-
-    # If you want to customize the save method to ensure some fields are automatically set
-    def save(self, commit=True):
-        instance = super(AskQuestionForm, self).save(commit=False)
-        if not instance.pk:  # If this is a new instance, set postDate
-            instance.postDate = timezone.now()
-            instance.user = self.fields['user']
-        if commit:
-            instance.save()
-            self.save_m2m()  # Required if the form has many-to-many relations
-        return instance
+        fields = ['user', 'title', 'content']
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'Enter your question title here'}),
+            'content': forms.Textarea(attrs={'placeholder': 'Write your question details here'}),
+        }
     
--- project/awtapp/views.py
+++ project/awtapp/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render, get_object_or_404
 from .forms import AskQuestionForm, RegistrationForm
 from .models import Answer, Question, Comment
 from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from .forms import AskQuestionForm
 
 def question_list(request):
     questions = Question.objects.all()
@@ -17,16 +19,13 @@ def question_detail(request, pk):
         answer_comments[answer] = Comment.objects.filter(answer=answer)
     return render(request, 'questions/question_detail.html', {'question': question, 'answers': answers, 'answer_comments': answer_comments})
 
+
 def ask_question(request):
     if request.method == 'POST':
         form = AskQuestionForm(request.POST)
         if form.is_valid():
-            title = form.cleaned_data['title']
-            content = form.cleaned_data['content']
-            # Create a new question object
-            question = Question.objects.create(title=title, content=content)
-            return redirect('question_detail', id=question.pk)
-
+            form.save()
+            return redirect('some_view_name')
     else:
         form = AskQuestionForm()
     return render(request, 'questions/ask_question.html', {'form': form})
