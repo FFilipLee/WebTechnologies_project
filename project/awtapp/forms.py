0c5f79aacefdb4424b398f25160112b53636ead2
Franz Lee <ff.lepka@gmail.com>
2024-04-23 01:21:31 +0200

    forms for crud

--- project/awtapp/forms.py
+++ project/awtapp/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Question, User
+from .models import Question, User, Answer, Comment
 from django.utils import timezone
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -21,3 +21,24 @@ class PostQuestionForm(forms.ModelForm):
             'content': forms.Textarea(attrs={'placeholder': 'Write your question details here'}),
         }
     
+class PostAnswerForm(forms.ModelForm):
+    user = forms.ModelChoiceField(queryset=User.objects.all(), label="Select User")
+    question = forms.ModelChoiceField(queryset=Question.objects.all(), label="Select Question")
+
+    class Meta:
+        model = Answer
+        fields = ['user', 'question', 'content']
+        widgets = {
+            'content': forms.Textarea(attrs={'placeholder': 'Write your answer here'}),
+        }
+
+class PostCommentForm(forms.ModelForm):
+    user = forms.ModelChoiceField(queryset=User.objects.all(), label="Select User")
+    answer = forms.ModelChoiceField(queryset=Answer.objects.all(), label="Select Answer")
+
+    class Meta:
+        model = Comment
+        fields = ['user', 'answer', 'content']
+        widgets = {
+            'content': forms.Textarea(attrs={'placeholder': 'Write your comment here'}),
+        }
