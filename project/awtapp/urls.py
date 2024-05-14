694bbcb54899f079fa7ba95520b5617a614f596d
Berkay Sahin <berkaysahindev@gmail.com>
2024-05-14 22:44:10 +0200

    fix bug

--- project/awtapp/urls.py
+++ project/awtapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path('questions/', views.question_list, name='question_list'),
-    path('questions/<int:pk>/', views.question_detail, name='question_detail'),
+    path('questions/<int:question_id>/', views.question_detail, name='question_detail'),
     path('questions/<int:question_id>/post_answer/', views.post_answer, name='post_answer'),
     path('answers/<int:answer_id>/post_comment/', views.post_comment, name='post_comment'),
     path('ask/', views.post_question, name='post_question'),
